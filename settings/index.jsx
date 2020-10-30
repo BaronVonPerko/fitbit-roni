8ab54936204bec9ed2466c0aead2e611d7239a9e
Chris Perko <chris@chrisperko.net>
2020-10-30 15:05:04 -0400

    Updated companion settings UI to show toggles for steps, heart rate, and calories (issue #4)

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,62 +1,69 @@
-import { APP_NAME, DATE_FORMATS, CLOCK_SIZES } from "../common/constants";
+import {APP_NAME, DATE_FORMATS, CLOCK_SIZES} from "../common/constants";
 
 function mySettings(props) {
-  return (
-    <Page>
-      <Section
-        title={
-          <Text bold align="center">
-            {`${APP_NAME} Settings`}
-          </Text>
-        }
-      >
-        <Toggle settingsKey="displaySeconds" label="Display Seconds" />
-        <Toggle settingsKey="displayBattery" label="Display Battery" />
-        <Select
-          label="Date Format"
-          settingsKey="dateFormat"
-          options={DATE_FORMATS.map(name => ({ name }))}
-        />
-        <Select
-          label="Clock Size"
-          settingsKey="clockSize"
-          options={CLOCK_SIZES.map(name => ({ name }))} />
-        <Text>UI Color</Text>
-        <ColorSelect
-          settingsKey="color"
-          colors={[
-            { color: "magenta" },
-            { color: "deepskyblue" },
-            { color: "aqua" },
-            { color: "aquamarine" },
-            { color: "coral" },
-            { color: "gold" },
-            { color: "hotpink" },
-            { color: "thistle" },
-            { color: "plum" },
-            { color: "lightsalmon" },
-            { color: "lightcoral" },
-            { color: "indianred" },
-            { color: "aliceblue" },
-            { color: "#f00" },
-            { color: "#0f0" }
-          ]}
-        />
-        <Text>
-          To see the heart rate monitor, simply tap anywhere on your watch
-          screen to toggle between steps and heart rate.
-        </Text>
-      </Section>
-      <Section>
-        <Link source="https://chrisperko.net/support-me">Support Me</Link>
-      </Section>
-      <Section>
-        <Text>Contributors:</Text>
-        <Link source="https://www.chrisperko.net">Chris Perko</Link>
-        <Link source="https://www.eliotlash.com/">Eliot Lash</Link>
-      </Section>
-    </Page>
-  );
+    return (
+        <Page>
+            <Section
+                title={
+                    <Text bold align="center">
+                        {`${APP_NAME} Settings`}
+                    </Text>
+                }
+            >
+                <Toggle settingsKey="displaySeconds" label="Display Seconds"/>
+                <Toggle settingsKey="displayBattery" label="Display Battery"/>
+                <Select
+                    label="Date Format"
+                    settingsKey="dateFormat"
+                    options={DATE_FORMATS.map(name => ({name}))}
+                />
+                <Select
+                    label="Clock Size"
+                    settingsKey="clockSize"
+                    options={CLOCK_SIZES.map(name => ({name}))}/>
+                <Text>UI Color</Text>
+                <ColorSelect
+                    settingsKey="color"
+                    colors={[
+                        {color: "magenta"},
+                        {color: "deepskyblue"},
+                        {color: "aqua"},
+                        {color: "aquamarine"},
+                        {color: "coral"},
+                        {color: "gold"},
+                        {color: "hotpink"},
+                        {color: "thistle"},
+                        {color: "plum"},
+                        {color: "lightsalmon"},
+                        {color: "lightcoral"},
+                        {color: "indianred"},
+                        {color: "aliceblue"},
+                        {color: "#f00"},
+                        {color: "#0f0"}
+                    ]}
+                />
+            </Section>
+
+            <Section>
+                <Text>
+                    To toggle between the following health metrics, simply tap anywhere on your watch!
+                </Text>
+                <Toggle settingsKey="displaySteps" label="Display Steps"/>
+                <Toggle settingsKey="displayHeartRate" label="Display Heart Rate"/>
+                <Toggle settingsKey="displayCalories" label="Display Calories"/>
+
+            </Section>
+
+            <Section>
+                <Link source="https://chrisperko.net/support-me">Support Me</Link>
+            </Section>
+            <Section>
+                <Text>Contributors:</Text>
+                <Link source="https://www.chrisperko.net">Chris Perko</Link>
+                <Link source="https://www.eliotlash.com/">Eliot Lash</Link>
+            </Section>
+        </Page>
+    );
 }
 
 registerSettingsPage(mySettings);
