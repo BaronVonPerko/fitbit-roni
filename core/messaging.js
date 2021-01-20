926104bdf1725718c002026ad1ed4590d04fb322
Chris Perko <chris@chrisperko.net>
2021-01-20 18:09:19 -0500

    Tabs converted to spaces

--- core/messaging.js
+++ core/messaging.js
@@ -5,55 +5,55 @@ import UI from "./ui";
 import Clock from './clock';
 import
 {
-	KEY_CLOCK_SIZE,
-	KEY_COLOR,
-	KEY_DISPLAY_SECONDS
+  KEY_CLOCK_SIZE,
+  KEY_COLOR,
+  KEY_DISPLAY_SECONDS
 } from "./common/constants";
 
 export default class Messaging
 {
-	static run()
-	{
-		const fileStore = FileStore.instance;
-		const ui = UI.instance;
-		
-		// Message socket opens
-		messaging.peerSocket.onopen = () =>
-		{
-			console.log("App Socket Open");
-		};
-		
-		// Message socket closes
-		messaging.peerSocket.onclose = () =>
-		{
-				console.log("App Socket Closed");
-		};
-		
-		messaging.peerSocket.onmessage = evt =>
-		{
-			console.log(evt.data.key + " : " + evt.data.newValue);
-			if (!evt.data.newValue) return;
-			const data = JSON.parse(evt.data.newValue);
-			fileStore.setValue(evt.data.key, data);
-			
-			switch (evt.data.key)
-			{
-				case KEY_COLOR:
-				{
-					ui.updateColor(data);
-					break;
-				}
-				case KEY_DISPLAY_SECONDS:
-				{
-					Clock.instance.setDisplaySeconds(data);
-					break;
-				}
-				case KEY_CLOCK_SIZE:
-				{
-					Clock.instance.setClockSize(data);
-					break;
-				}
-			}
-		};
-	}
+  static run()
+  {
+    const fileStore = FileStore.instance;
+    const ui = UI.instance;
+
+    // Message socket opens
+    messaging.peerSocket.onopen = () =>
+    {
+      console.log("App Socket Open");
+    };
+
+    // Message socket closes
+    messaging.peerSocket.onclose = () =>
+    {
+        console.log("App Socket Closed");
+    };
+
+    messaging.peerSocket.onmessage = evt =>
+    {
+      console.log(evt.data.key + " : " + evt.data.newValue);
+      if (!evt.data.newValue) return;
+      const data = JSON.parse(evt.data.newValue);
+      fileStore.setValue(evt.data.key, data);
+
+      switch (evt.data.key)
+      {
+        case KEY_COLOR:
+        {
+          ui.updateColor(data);
+          break;
+        }
+        case KEY_DISPLAY_SECONDS:
+        {
+          Clock.instance.setDisplaySeconds(data);
+          break;
+        }
+        case KEY_CLOCK_SIZE:
+        {
+          Clock.instance.setClockSize(data);
+          break;
+        }
+      }
+    };
+  }
 }
