798d31cac5014693b3cc7120bfcb30a6a4423ef5
Chris Perko <chris@chrisperko.net>
2021-01-22 17:38:31 -0500

    Code fixes to get 5.0 project to build

--- 5.0/app/index.js
+++ 5.0/app/index.js
@@ -1,26 +1,14 @@
-import Battery from "../../inc/battery";
-import Clock from "../../inc/clock";
-import Messaging from "../../inc/messaging";
-import UI from "../../inc/ui";
-import FileStore from "../../inc/fileStore";
+import Battery from "../../core/battery";
+import Clock from "../../core/clock";
+import Messaging from "../../core/messaging";
+import UI from "../../core/ui";
+import FileStore from "../../core/fileStore";
 
-//import analytics from "fitbit-google-analytics/app";
+import analytics from "fitbit-google-analytics/app";
 
-/*analytics.configure(
-{
+analytics.configure({
   tracking_id: "UA-26651291-17"
-});*/
-
-import
-{
-  me as device
-} from "device";
-
-if (!device.screen) device.screen =
-  {
-    width: 348,
-    height: 250
-  };
+});
 
 UI.instance.restore();
 
